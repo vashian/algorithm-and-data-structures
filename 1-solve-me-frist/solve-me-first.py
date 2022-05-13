325aa5b947300591bd829302672526e4eee34282
Mohammadreza <mrvashian@gmail.com>
2022-05-13 20:19:09 +0430

    solve me first

---
+++ 1-solve-me-frist/solve-me-first.py
@@ -0,0 +1,13 @@
+# get inputs
+a = int(input().split(" = ")[-1])
+b = int(input().split(" = ")[-1])
+
+# function to sum two numbers
+
+
+def solve_me_first(a, b):
+    return a + b
+
+
+# print the result
+print(solve_me_first(a, b))
